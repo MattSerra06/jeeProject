fb72cb63fe143ec62ca8cc0d7185db29f82ae40e
Matthieu Serra <matthieuserra06@gmail.com>
2023-11-06 15:23:53 +0100

    Cors config

---
+++ jeeProject/src/main/java/com/jeeproject/jeeProject/spring/config/CorsConfig.java
@@ -0,0 +1,20 @@
+package com.jeeproject.jeeProject.spring.config;
+
+import org.springframework.context.annotation.*;
+import org.springframework.web.cors.*;
+import org.springframework.web.filter.*;
+
+@Configuration
+public class CorsConfig {
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOriginPattern("*"); // Autorise l'accès depuis n'importe quelle origine
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
+}
